6891f734f83a527502559c9b7eacb58b787717af
= <=>
2019-04-16 15:28:26 +0800

    test

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ let path = require("path");
 let bodyParser = require('body-parser');
 
 
-// 解析 application/json
-app.use(bodyParser.json()); 
-// 解析 XML
-app.use(bodyParser.text({ type: 'text/xml' }))
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.text({ type: 'text/plain' }))
+
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set( 'view engine', 'ejs' );
@@ -44,7 +52,7 @@ app.post('/air', function( req, res){
   console.log( req );
   res.status = 200;
   res.send("hello")
-  
+
 })
 
 
