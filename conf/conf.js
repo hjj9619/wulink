c47ed382bc2e8bf18fb172dc5b2453b535d966f4
= <=>
2019-04-16 12:52:51 +0800

    change deviceUrl

--- conf/conf.js
+++ conf/conf.js
@@ -1,6 +1,6 @@
 exports = module.exports = {
     "appKey": "7FVaHrZvTNrFyMG1uoyF7QsKyRKa0746",
     "appId": "78f4dfd35418565e20c67997b96ed793",
-    "accessUrl": "https://api.ifanscloud.com/v3/access/token",
-    "deviceUrl": "https://api.ifanscloud.com/v3/device"
+    "accessUrl": "https://api.ifanscloud.com/v3/access/token/",
+    "deviceUrl": "https://api.ifanscloud.com/v3/device/"
 }
