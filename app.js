c7ce5d1031825486ee58d348cbe0a2af4e0d201d
= <=>
2019-04-16 12:35:28 +0800

    获取悟空的在线状态

--- app.js
+++ app.js
@@ -2,13 +2,43 @@ let express = require("express");
 let app = express();
 let getLocalToken = require('./util/getAccessToken').getLocalToken;
 let getAccessToken = require('./util/getAccessToken').getAccessToken;
+let conf = require('./conf/conf');
+let request = require('request');
+let md5 = require('md5');
+
+
 app.listen(3000);
 
 
-getAccessToken();
+// getAccessToken();
 
-setTimeout(function(){
-  getLocalToken().then(function(data){
-    console.log( data );
+
+getLocalToken().then(function(data){
+  // console.log( data );
+  let now = new Date().getTime();
+  let SIGN = md5(conf.appId + data + "asdfghjkllkjhgfdsa", now)
+  let formData = {
+    appid: conf.appId,
+    nonce: "asdfghjkllkjhgfdsa",
+    timestamp: now,
+    sign: SIGN,
+    devtype: "Wukong",
+    cmdtype: "get",
+    cmd: 'online'
+
+  }
+  let url = conf.deviceUrl + "808600016928";
+  request.get(url, { formData: formData }, function( err, response, body ){
+    console.log( body );
   })
-}, 2000);
+
+
+}).catch(function( err ){
+  if( err ) console.log( err );
+})
+
+/**
+ * 
+ * 80238201?appid=APPID&nonce=NONCE&timestamp=TIMESTAMP&sign=SIGN&devtype=DEVTYPE&cmdtype=GET&cmd=online
+ * 
+ */
