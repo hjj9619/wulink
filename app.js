157f95fb0caae264c3065e4f8e7191b053fac1d8
= <=>
2019-04-16 13:48:00 +0800

    整理文件结构

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let express = require("express");
 let app = express();
-let getLocalToken = require('./util/getAccessToken').getLocalToken;
 let getAccessToken = require('./util/getAccessToken').getAccessToken;
-let conf = require('./conf/conf');
-let request = require('request');
-let md5 = require('md5');
+let getDeviceInfo = require('./libs/ctrl').getDeviceInfo;
+
+
 
 
 app.listen(3000);
@@ -13,41 +12,5 @@ app.listen(3000);
 // getAccessToken();
 
 
-getLocalToken().then(function(data){
-  // console.log( data );
+getDeviceInfo('808600016928', 'get', 'air_ctrl');
 
-  let now = new Date().getTime();
-  let nonce = "ASaSJLLJIOqeoiaq"
-  let SIGN = md5(conf.appId + data + nonce + now);
-  let formData = {
-    "appid": conf.appId,
-    "nonce": nonce,
-    "timestamp": now,
-    "sign": SIGN,
-    "devtype": "Wukong",
-    "cmdtype": "get",
-    "cmd": "device_info"
-  }
-
-  let str = JSON.stringify( formData );
-  let url = conf.deviceUrl + "808600016928?" + `appid=${ conf.appId }&nonce=ASaSJLLJIOqeoiaq&timestamp=${ now }&sign=${ SIGN }&devtype=Wukong&cmdtype=get&cmd=${ "air_ctrl" }` ;
-  let url2 = conf.deviceUrl + "808600016928";
-  request.get(url, {json: true}, function( err, response, body ){
-    if( !err ){
-      // console.log( response );
-      console.log( body );
-    }else{
-      console.log( err );
-    }
-  })
-
-
-}).catch(function( err ){
-  if( err ) console.log( err );
-})
-
-/**
- * 
- * 80238201?appid=APPID&nonce=NONCE&timestamp=TIMESTAMP&sign=SIGN&devtype=DEVTYPE&cmdtype=GET&cmd=online
- * 
- */
---
+++ libs/ctrl.js
@@ -0,0 +1,48 @@
+let request = require('request');
+let md5 = require('md5');
+let conf = require('../conf/conf');
+let getLocalToken = require('../util/getAccessToken').getLocalToken;
+
+
+
+function getDeviceInfo ( id, cmdtype, cmd ){
+    getLocalToken().then(function(data){
+        // console.log( data );
+      
+        let now = new Date().getTime();
+        let nonce = "ASaSJLLJIOqeoiaq"
+        let SIGN = md5(conf.appId + data + nonce + now);
+
+
+        // let formData = {
+        //   "appid": conf.appId,
+        //   "nonce": nonce,
+        //   "timestamp": now,
+        //   "sign": SIGN,
+        //   "devtype": "Wukong",
+        //   "cmdtype": "get",
+        //   "cmd": "device_info"
+        // }
+        // let url2 = conf.deviceUrl + "808600016928";
+        // let str = JSON.stringify( formData );
+
+        let url = conf.deviceUrl + id + `?appid=${ conf.appId }&nonce=ASaSJLLJIOqeoiaq&timestamp=${ now }&sign=${ SIGN }&devtype=Wukong&cmdtype=${ cmdtype }&cmd=${ cmd }` ;
+        
+        request.get(url, {json: true}, function( err, response, body ){
+          if( !err ){
+            console.log( body );
+          }else{
+            console.log( err );
+          }
+        })
+      
+      
+      }).catch(function( err ){
+        if( err ) console.log( err );
+      })
+}
+
+
+exports = module.exports = {
+    getDeviceInfo: getDeviceInfo
+}
